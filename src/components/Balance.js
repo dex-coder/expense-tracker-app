2db484f3c2c3c78567d3abf3036f62c43baa659c
dex-coder <mhdkashifkhan028@gmail.com>
2021-01-25 19:57:03 +0500

    updated Balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,33 +2,16 @@ import React, { useContext } from 'react'
 import { GlobalContext } from "../context/GlobalState";
 
 
-//Money formatter function
-function moneyFormatter(num) {
-    let p = num.toFixed(2).split('.');
-    return (
-      '$ ' +
-      p[0]
-        .split('')
-        .reverse()
-        .reduce(function (acc, num, i, orig) {
-          return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
-        }, '') +
-      '.' +
-      p[1]
-    );
-  }
-
-
 export const Balance = () => {
     const { transactions } = useContext(GlobalContext)
     //console.log(transactions)
     const amount =  transactions.map(transaction => transaction.amount)
-    const total = amount.reduce((acc, item) => (acc =+ item), 0);
+    const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
     console.log(total)
     return (
         <div>
             <h4>Your Balance</h4>
-            <h1 id="balance">{moneyFormatter(total)}</h1>
+            <h1 id="balance">${total}</h1>
         </div>
     )
 }
